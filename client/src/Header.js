9c8175ecb12e9dd659f3059db7eb3484bdafba25
Anthony <andeng2@berkeley.edu>
2023-09-09 22:07:40 -0700

    server and client (min style)

--- client/src/Header.js
+++ client/src/Header.js
@@ -5,28 +5,28 @@ import {UserContext} from "./UserContext";
 export default function Header() {
   const {setUserInfo,userInfo} = useContext(UserContext);
   useEffect(() => {
-    fetch('http://localhost:3000/profile', {
+    fetch('http://localhost:3001/profile', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
         setUserInfo(userInfo);
-      })
-    })
-  }, [])
+      });
+    });
+  }, []);
 
   function logout() {
-    fetch('http://localhost:3000/logout', {
+    fetch('http://localhost:3001/logout', {
       credentials: 'include',
       method: 'POST',
-    })
-    setUserInfo(null)
+    });
+    setUserInfo(null);
   }
 
   const username = userInfo?.username;
 
   return (
     <header>
-      <Link to="/" className="logo">Between</Link>
+      <Link to="/" className="logo">MyBlog</Link>
       <nav>
         {username && (
           <>
